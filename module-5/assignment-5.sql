65b95e86018bc424310de51c20da79956c391964
= <=>
2023-05-15 16:52:20 -0700

    Started assignment-5.

---
+++ module-5/assignment-5.sql
@@ -0,0 +1,18 @@
+/**************************************************
+** File: Assignment 5
+** Date: 2023-05-15
+** Auth: Ramkumar Rajanbabu
+***************************************************
+** Desc: Module 5 - Assignment 5
+***************************************************
+** Modification History
+***************************************************
+** Date			Author				Description 
+** ----------	------------------  ---------------
+** 2023-05-15	Ramkumar Rajanbabu	Started assignment
+**************************************************/
+
+-- Access Database
+USE [AdventureWorks2019]
+GO
+
